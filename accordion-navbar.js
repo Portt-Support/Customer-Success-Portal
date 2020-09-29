75878c1a87e942eee017ffeb18c7a79ceaad0937
Matthew H <mitazad@gmail.com>
2020-09-29 11:41:44 +1000

    Update accordion-navbar.js

--- accordion-navbar.js
+++ accordion-navbar.js
@@ -1,3 +1,14 @@
+
+
+// navbar utility script
+//Like all my other scripts
+//Dont bother maintaining just start again
+//Written By: Not Matthew Holmes
+
+
+//Added this to try and force GC to reload 
+console.log("Version 1.0.2");
+
 var navbar_elements = document.getElementsByClassName("topic-block");
 
 
