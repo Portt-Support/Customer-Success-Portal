afdc1ee37fd89f783379c9398a721d5b22efaf43
Matthew H <mitazad@gmail.com>
2020-09-29 15:22:09 +1000

    Update accordion-navbar.js
    Rewrote the script to be simpler and work in more situations

--- accordion-navbar.js
+++ accordion-navbar.js
@@ -8,42 +8,39 @@
 
 
 //Added this to try and force GC to reload 
-console.log("Version 1.0.4");
+console.log("Accordion Navbar - Version 1.1.0");
 
-var navbar_elements = document.getElementsByClassName("topic-block");
+//store all topic blocks once at page load
+  var topicBlocks = document.getElementsByClassName("topic-block");
 
 
-function navbar_check(){
-
-	navbar_elements = document.getElementsByClassName("topic-block");
-
-	for(block of navbar_elements){
-  
-		var targetTopic = block.firstChild.className;  
-  
-  	var targetClass = block.className;
-    var topic = "sub-link-wrapper " + targetClass.replace("topic-block ", "");
-    //checks if container is currently in view port
-    //When a link references a specific class in WebFlows
-    //"w--current" is appended to the classname when in view
-    if(targetTopic.includes("w--current")){
-			var links = document.getElementsByClassName(topic);
+	//function used to determine what nav bar items should be shown or hidden
+  function navbar_check(){
+		
+    //loop over every topic block found on the page
+    for(topicBlock of topicBlocks){
       
-      for(link of links){
-      	
-        link.style.display = "block";
+      //Gets the first nav_link in the topic block
+      //The first nav_link in the topic block points to the container for the entire topic
+      var className = topicBlock.getElementsByClassName("nav_link")[0].className;
+      
+      //if the first nav_link in the topic block is active, show all sub links
+      //else we hide all sub links
+   		if (className == "nav_link w--current"){
+        //I guess it's possible that there are multiple wrapps found in the same topic block
+        //To get around this we store the list and loop through it
+        for(subWrapper of topicBlock.getElementsByClassName("sub-link-wrapper")){
+        	subWrapper.style.display = "block";
+        }
       }
-		}
-    else{
-    	var links = document.getElementsByClassName(topic);
-      for(link of links){
-        link.style.display = "none";
+      else{
+      	for(subWrapper of topicBlock.getElementsByClassName("sub-link-wrapper")){
+        	subWrapper.style.display = "none";
+        }
       }
+        
     }
-  	
+    setTimeout(navbar_check, 100);
   }
-  
-  setTimeout(navbar_check, 200);
-}
 
-navbar_check();
+  navbar_check();	
